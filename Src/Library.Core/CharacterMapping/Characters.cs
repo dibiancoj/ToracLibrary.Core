48fb403458054e1f475779adead93742da0a4a01
Jason <dibiancoj@gmail.com>
2019-01-03 13:28:37 -0500

    add the character mapping functionality

---
+++ Src/Library.Core/CharacterMapping/Characters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Core.CharacterMapping
+{
+
+    /// <summary>
+    /// Holds a list for alphabet and numeric characters
+    /// </summary>
+    public static class Characters
+    {
+
+        #region Constants
+
+        /// <summary>
+        /// Holds the alphabet in a string. 
+        /// </summary>
+        public const string AlphabetCharacters = "abcdefghijklmnopqrstuvwxyz";
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the number characters in the english language
+        /// </summary>
+        /// <returns>all the numbers in the english language in an iterator</returns>
+        public static IEnumerable<int> AllNumberCharactersLazy()
+        {
+            //loop through the numbers and yield them
+            for (int i = 0; i < 10; i++)
+            {
+                //return i
+                yield return i;
+            }
+        }
+
+
+        public static IEnumerable<char> AllAlphaBetCharactersString() => AlphabetCharacters;
+
+        /// <summary>
+        /// Returns the alphabet characters in the english language
+        /// </summary>
+        /// <returns>all the alphabet characters in the english language</returns>
+        public static ReadOnlySpan<char> AllAlphaBetCharacters() => AlphabetCharacters.AsSpan();
+
+        #endregion
+
+    }
+
+}
---
+++ Test/Library.Core.Tests/CharacterMapping/CharacterMappingTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using static Library.Core.CharacterMapping.Characters;
+
+namespace Library.Core.Tests.CharacterMapping
+{
+
+    /// <summary>
+    /// Unit test to test the character mapping functionality
+    /// </summary>
+    public class CharacterMappingTest
+    {
+
+        [Fact(DisplayName = "Make sure the numeric numbers return are correct")]
+        public void NumericCharactersTest1()
+        {
+            //going to hard code this test
+            var digitsToTest = AllNumberCharactersLazy().OrderBy(x => x).ToArray();
+
+            //start testing this. going to do this manually to ensure everything is correct
+            for (int i = 0; i < 10; i++)
+            {
+                //test the result
+                Assert.Equal(i, digitsToTest[i]);
+            }
+        }
+
+        [Fact(DisplayName = "Make sure the alphabet numbers return are correct")]
+        public void AlphabetCharactersTest1()
+        {
+            //loop through all the characters and test it
+            var resultOfCall = AllAlphaBetCharactersString();
+
+            //holds an independent string incase characters.constant gets modified by accident
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            //we are first going to test the constant in the characters.cs module to make sure we are in sync. 
+            Assert.Equal(alphabet, AlphabetCharacters);
+
+            //test all the characters now (we are not going to call Characters
+            foreach (var requiredCharacter in alphabet)
+            {
+                //make sure its there
+                Assert.Contains(requiredCharacter, resultOfCall);
+            }
+        }
+
+    }
+
+}
