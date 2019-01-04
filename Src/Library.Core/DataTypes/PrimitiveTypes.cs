a958ece50a3340d40ed81d7712fccf52298dd331
Jason DiBianco <dibiancoj@gmail.com>
2019-01-04 14:50:34 -0500

    DataTypes - Primitive TYpes

---
+++ Src/Library.Core/DataTypes/PrimitiveTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Library.Core.DataTypes
+{
+
+    /// <summary>
+    /// Gives you the base types. The PropertyInfo.PropertyType.IsPrimitive doesnt give you string and the nullable types. This class will give you all the primitive types include nullable values
+    /// </summary>
+    public static class PrimitiveTypes
+    {
+
+        /// <summary>
+        /// Gives you the base types. The PropertyInfo.PropertyType.IsPrimitive doesnt give you string and the nullable types. This class will give you all the primitive types include nullable values
+        /// </summary>
+        /// <returns>List Of Types</returns>
+        public static ImmutableHashSet<Type> PrimitiveTypeLookup { get; } = ImmutableHashSet.Create(
+            typeof(string),
+            typeof(bool),
+            typeof(bool?),
+            typeof(DateTime),
+            typeof(DateTime?),
+            typeof(Int16),
+            typeof(Int16?),
+            typeof(Int32),
+            typeof(Int32?),
+            typeof(Int64),
+            typeof(Int64?),
+            typeof(double),
+            typeof(double?),
+            typeof(float),
+            typeof(float?),
+            typeof(decimal),
+            typeof(decimal?)
+            );
+
+    }
+
+}
---
+++ Test/Library.Core.Tests/DataTypes/DataTypesTest.cs
@@ -0,0 +1,60 @@
+﻿using Library.Core.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Library.Core.Tests.DataTypes
+{
+
+    /// <summary>
+    /// Unit test to test data types
+    /// </summary>
+    public class DataTypesTest
+    {
+
+        #region Primitive Types
+
+        /// <summary>
+        /// Test Primitive types. All these items should be found in the list
+        /// </summary>
+        [InlineData(typeof(string))]
+        [InlineData(typeof(bool))]
+        [InlineData(typeof(bool?))]
+        [InlineData(typeof(Int16))]
+        [InlineData(typeof(Int16?))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(int?))]
+        [InlineData(typeof(Int64))]
+        [InlineData(typeof(Int64?))]
+        [InlineData(typeof(double))]
+        [InlineData(typeof(double?))]
+        [InlineData(typeof(float))]
+        [InlineData(typeof(float?))]
+        [InlineData(typeof(decimal))]
+        [InlineData(typeof(decimal?))]
+        [Theory]
+        public void PrimitiveTypesTest1(Type TypeToTest)
+        {
+            //make sure this is in the list
+            Assert.Contains(TypeToTest, PrimitiveTypes.PrimitiveTypeLookup);
+        }
+
+        /// <summary>
+        /// Test Primitive types. All these items should NOT be found in the list
+        /// </summary>
+        [InlineData(typeof(IEnumerable<double>))]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(List<double>))]
+        [Theory]
+        public void PrimitiveTypesTest2(Type TypeToTest)
+        {
+            //make sure this item is NOT in the list
+            Assert.DoesNotContain(TypeToTest, PrimitiveTypes.PrimitiveTypeLookup);
+        }
+
+        #endregion
+
+    }
+
+}
